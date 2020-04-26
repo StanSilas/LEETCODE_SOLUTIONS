7b2e1bdd4f350fde9e107702bed53fcf140dff82
Stan Silas(Vivek Mangipudi) <viv4864@gmail.com>
2020-04-25 22:56:36 -0400

    max_product

--- IC/max_product.py
+++ IC/max_product.py
@@ -1,2 +1,9 @@
 # Given a list of integers, find the highest product you can get from three of the integers.
 
+list_of_ints = [ 1, 1, 8, 9, 4]
+
+def highest_product(mylist):
+  sorted_list = list_of_ints.sort(reverse = True)
+  return list_of_ints[0]*list_of_ints[1]*list_of_ints[2]
+
+print (highest_product(list_of_ints))
