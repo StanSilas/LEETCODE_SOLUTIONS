7c4a5b6ac73ed147bd672a91d9060ed0a4db7d3a
Stan Silas(Vivek Mangipudi) <viv4864@gmail.com>
2020-04-28 16:15:25 -0400

    Update take2_3max_prod.py
    added algo

--- IC/take2_3max_prod.py
+++ IC/take2_3max_prod.py
@@ -1,1 +1,9 @@
-test
+Sort the list in ascending order. 
+
+1 compute the max product from the tail 
+
+compute the max product of the two numbers at 1 end and the biggest number towards the right end. 
+
+One of these two would be the largest possible product. 
+
+Save the values.
