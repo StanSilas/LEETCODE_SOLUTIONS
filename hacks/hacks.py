462fa2111ab853dfd093d305f6dccb2497295397
Stan Silas(Vivek Mangipudi) <viv4864@gmail.com>
2020-04-29 13:58:04 -0400

    common hacks python
    common hacks

---
+++ hacks/hacks.py
@@ -0,0 +1,22 @@
+# sort a list
+list_of_ints.sort(reverse = True)
+
+# Remove duplicates from a list by converting to a dict
+list(dict.fromkeys(my_list_x))
+
+# Difference between two lists 
+(list(set(list1) - set(list2)))
+
+# Reverse a string 
+def my_function(x):
+  return x[::-1]
+  
+# Iterate over a list of numbers
+list = [1, 3, 5, 7, 9] 
+for i in list: 
+    print(i)   
+
+# Iterate using the index
+
+for i in range(len(nums)):
+  do_something
