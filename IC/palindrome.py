6921a9b04ca0c19d66cc1d0b779940142c9609b1
Stan Silas(Vivek Mangipudi) <viv4864@gmail.com>
2020-05-04 00:58:23 -0400

    Update palindrome.py
    simpler pallindrome logic of reverse adding i+x instead of x+i

--- IC/palindrome.py
+++ IC/palindrome.py
@@ -1,12 +1,18 @@
 def palindrome(my_string):
     x = ""
-    for i in range(len(my_string)+1):
-        x = x + my_string[-i]
+    for i in my_string:
+        x = i + x # form the string in reverse
+       
+    # for i in range(len(my_string)):
+    #     x = x + my_string[-i]
 
     if x == my_string:
         print("palindrome")
+        print(x)
     else:
         print("not palindrome")
+        print(x)
     
 
 palindrome("Hello, World")
+# palindrome("AbbA")
